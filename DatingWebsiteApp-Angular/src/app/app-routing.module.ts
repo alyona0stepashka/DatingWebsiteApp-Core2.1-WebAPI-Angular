4d0cdba7cbd19779c21612653b6c83d77517f74c
alyona0stepashka <alyona0stepashka@users.noreply.github.com>
2019-05-31 21:58:56 +0300

    omg im dead

--- DatingWebsiteApp-Angular/src/app/app-routing.module.ts
+++ DatingWebsiteApp-Angular/src/app/app-routing.module.ts
@@ -3,6 +3,8 @@ import { Routes, RouterModule } from '@angular/router';
 import { AuthComponent } from './components/auth/auth.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
+import { AuthGuard } from './guards/auth.guards';
+import { SearchComponent } from './components/home/search/search.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/auth/login', pathMatch:'full'},
@@ -10,9 +12,10 @@ const routes: Routes = [
     path:'auth', component: AuthComponent,
     children:[
       {path:'login', component:LoginComponent},
-      {path:'register',component:RegisterComponent}
+      {path:'register', component:RegisterComponent}
     ]
-  }
+  },
+  {path:'search', component: SearchComponent,canActivate:[AuthGuard]}
 ];
 
 @NgModule({
--- DatingWebsiteApp-Angular/src/app/app.module.ts
+++ DatingWebsiteApp-Angular/src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
@@ -6,19 +7,43 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './components/auth/login/login.component';
 import { RegisterComponent } from './components/auth/register/register.component';
 import { AuthComponent } from './components/auth/auth.component';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
+import { AuthInterceptor } from './guards/auth.interceptor';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { ToastrModule } from 'ngx-toastr';
+import { HomeComponent } from './components/home/home.component';
+import { RouterModule } from '@angular/router';
+import { SearchComponent } from './components/home/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
     RegisterComponent,
-    AuthComponent
+    AuthComponent,
+    HomeComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    CommonModule,
+    BrowserAnimationsModule,
+    ToastrModule.forRoot()
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: AuthInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
